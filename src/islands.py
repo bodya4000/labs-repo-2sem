a16c69264c2da87e342fe96b63fbf3213afa1376
Bohdan <151460276+bodya4000@users.noreply.github.com>
2024-05-07 11:21:30 +0300

    changed implementation to warshall algo

--- src/islands.py
+++ src/islands.py
@@ -1,54 +1,59 @@
 import heapq
+import math
 
-
-class MinPriorityQueue:
-    def __init__(self):
-        self._queue = []
-
-    def push(self, item, priority):
-        heapq.heappush(self._queue, (priority, item))
-
-    def pop(self):
-        return heapq.heappop(self._queue)
-
-    def __len__(self):
-        return len(self._queue)
-
-
-def write_output(output_file_path, result):
-    file = open(output_file_path, 'w')
-    file.write(str(result))
-    file.close()
-
-
-def read_csv_and_return_matrix(input_file):
-    matrix = []
+def read_csv_and_return_matrix(input_file: str) -> list[list[int]]:
+    """
+    reads adj_matrix from file
+    """
+    adj_matrix = []
     file = open(input_file, 'r')
     try:
         for line in file:
             neighbours = list(map(int, line.split(', ')))
-            matrix.append(neighbours)
+            adj_matrix.append(neighbours)
         file.close()
-        return matrix
+        return adj_matrix
     except ValueError:
         file.close()
-        return
+        return [[]]
 
 
-def prims_algorithm(matrix):
-    visited = set()
-    priority_queue = MinPriorityQueue()
-    total_len = 0
-    priority_queue.push(0, 0)
+def warshall(adjacency_matrix: list[list[int]]) -> int:
+    num_islands = len(adjacency_matrix)
+    total_length = 0
+    for k in range(num_islands):
+        for i in range(num_islands):
+            for j in range(num_islands):
+                shortest_path_via_k = adjacency_matrix[i][k] + adjacency_matrix[k][j]
+                current_path = adjacency_matrix[i][j]
+                if shortest_path_via_k == 0 or current_path == 0:
+                    continue
+                adjacency_matrix[i][j] = min(current_path, shortest_path_via_k)
+                if k == num_islands - 1:
+                    total_length += current_path
+    return total_length
 
-    while len(visited) < len(matrix):
-        path, current_row = priority_queue.pop()
-        if current_row not in visited:
-            visited.add(current_row)
-            total_len += path
-            for i in range(len(matrix)):
-                if matrix[current_row][i] != 0 and i not in visited:
-                    priority_queue.push(i, matrix[current_row][i])
 
-    return total_len
+def iter_current_row(current_row, matrix, priority_queue, visited):
+    """
+    iterates in row of matrix
+    and adds all nodes in it to priority queue
+    """
+    for i in range(len(matrix)):
+        if vertex_has_not_path_or_not_in_visited(current_row, i, matrix, visited):
+            priority_queue.push(i, matrix[current_row][i])
 
+
+def vertex_has_not_path_or_not_in_visited(current_row, i, matrix, visited):
+    """
+    check if current node is not in visited
+    :returns bool
+    """
+    return matrix[current_row][i] != 0 and i not in visited
+
+
+if __name__ == '__main__':
+    matrix_from_file = read_csv_and_return_matrix("../resources/islands/islands_input.csv")
+    matrix = [[0, 3, math.inf, 7], [8, 0, 2, math.inf], [5, math.inf, 0, 1], [2, math.inf, math.inf, 0]]
+    print(warshall(matrix_from_file))
+    print(matrix_from_file)
